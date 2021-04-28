24d897488f785c6c41bd9b7bc30d889a94024c9b
Marcin <zolkiewski.marcin@gmail.com>
2021-04-28 22:02:35 +0200

    debug change

--- src/api/controllers/user/oauth.ts
+++ src/api/controllers/user/oauth.ts
@@ -6,6 +6,8 @@ import { userService } from '../../services';
 const oauth: RequestHandler = async (req, res) => {
   const user = await userService.oauth(req.body.code, req.body.state);
 
+  console.log('$$$$$$$$$$$$$$$$ pre response', user);
+
   res.send(user);
 };
 
